2cc16751bcc7d5f57607e6a49670c4cbb27b2ed8
artpolo04 <artpolo04@gmail.com>
2023-06-09 20:08:26 -0500

    fisrt commit

---
+++ src/main/java/com/mycompany/maven_final_final/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.maven_final_final;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
